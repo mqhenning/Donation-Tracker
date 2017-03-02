253f453fccf4f5034cb159bd77d94d9872121ea0
Michael Henning <mhenning@CHIC02QX1MBG8WN.sea.corp.expecn.com>
2017-03-02 13:38:54 -0600

    Lets pass the URL as a param, and include some failure resiliance

--- Donation Tracker.py
+++ Donation Tracker.py
@@ -1,15 +1,25 @@
 from lxml import html
 import requests
 import threading
+
+
 #Campaign URL goes here
-def timeloop():
+def timeloop(url):
     threading.Timer(60.0, timeloop).start()
-    page = requests.get('http://www.ppaction.org/site/TR?px=9693618&fr_id=1160&pg=personal')
-    tree = html.fromstring(page.content)
-    #This will find the goal amount
-    goal = tree.xpath('//span[@class="amount-raised-value"]/text()')
-    goal = [item.replace("\n", "") for item in goal]
+    page = requests.get(url)
+    if page.status_code == 200:
+        tree = html.fromstring(page.content)
+        #This will find the goal amount
+        goal = tree.xpath('//span[@class="amount-raised-value"]/text()')
+        goal = [item.replace("\n", "") for item in goal]
+    else:
+        print("Page responded with "+repr(page.status_code))
     #This prints the currently raised amount out of the total goal
     with open("donation.txt", "w") as text_file:
         print("".join(map(str, goal)), '/$10,000', sep="", file=text_file)
-timeloop()
+
+
+
+if __name__ == "__main__":
+    url = 'http://www.ppaction.org/site/TR?px=9693618&fr_id=1160&pg=personal'
+    timeloop(url)
